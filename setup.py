27750aa2b2a5626e765bc98cc97599ead8b1832a
brainfo <1248276548@qq.com>
2024-09-08 12:51:01 +0200

    v1 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
   
 setup(
     name='cscore',
-    version='0.1',
+    version='1',
     description='Scoring the commonness from differential analysis',
     author='Hong Jiang',
     author_email='scilavisher@gmail.com',
